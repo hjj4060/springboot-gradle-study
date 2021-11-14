40763fe35986d64322ec86be9c1f4c9ed810400d
hjj4060 <rkdgustn77@naver.com>
2021-11-15 04:31:42 +0900

    2021년 11월 15일 정적 컨텐츠, MVC와 템플릿 엔진 강의 학습

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // 웹 어플리케이션에서 첫번째 진입점이 Controller 이다.
 @Controller
@@ -13,5 +14,12 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data","hello!!"); // data는 model을 통해서 넘긴다.
         return "hello"; // resources/template/hello.html를 찾아서 랜더링하게 된다.
+    }
+
+    @GetMapping("hello-mvc")
+    // command + P로 파라미터 옵션 볼수 있음 required를 false로 하면 파라미터 없어도 됨
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
